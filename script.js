4c8a133ae0c6714ca2bf421b6afeb15d32c70b1d
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-11-27 10:48:12 +0100

    feat : Declaration des functions (vide) + commentaires

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
 
-/*
+let listeTickets = []
 
-fetch('https://webhelprequest.deta.dev/users')
-.then(response => response.json()) 
-.then(json => console.log(json));*/
+/** Permet d'ajouter un utilisateur dans l'API */
+function ajoutUtilisateur(nom){
+    const addUser = {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({ username: nom, password: "none" })
+    }
+    fetch(`https://webhelprequest.deta.dev/users`, addUser)
+    .then(response => response.json())
+    .then(response => console.log(response)) 
+}
 
-getUsers()
 
-function getUsers() {
+
+/** Récupération des utilisateurs depuis l'API et remplir la variable globale listeTickets */
+function recupUtilisateurs() {
     fetch(`https://webhelprequest.deta.dev/users`)
     .then(response => response.json())
     .then(response => console.log(response));
 
 }
 
-/* const addUser = {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({ username: "Johann", password: "none" })
+/** Remplir la ComboBox avec les nom des utilisateurs */
+function remplirComboBox(listeUtilisateurs){
+
 }
-fetch(`https://webhelprequest.deta.dev/users`, addUser)
-    .then(response => response.json())
-    .then(response => console.log(response)) */
+
+/** Ajout d'un nouveau ticket par un utilisateur */
+function nouveauTicket(){
+
+}
+
+
+/** Permet d'ajouter un ticket dans l'API  et dans la variable globale listeTickets*/
+function ajoutTicket(){
+
+}
+
+/** Rafraichi l'affichage des tickets */
+function rafraichirTicket(){
+
+}
+
+/** Permet de "supprimer" le premier ticket dans l'API et dans la variable globale listeTickets*/
+function supprimTicket(){
+
+}
+
+/** Inverse un ticket avec le suivant */
+function switchTicket(){
     
+}
+
+
+recupUtilisateurs()
+
+
+
+
+
 
 
